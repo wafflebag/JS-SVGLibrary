ba765d576f91d0b76684cf80c695b1fa85527516
wafflebag <40217899+wafflebag@users.noreply.github.com>
2020-01-02 17:39:07 -0800

    Update sight.js

--- js/sight.js
+++ js/sight.js
@@ -2,5 +2,18 @@ class SVGElement{
     constructor(type){
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(this.namespace, this.type);
+        return this;
+    }
+    attr(attrs) {
+        for (const [key, value] of Object.entries(attrs)) {
+            this.node.setAttributeNS(null, key, value);
+        }
+        return this;
+    }
+    append(element) {
+        const parent = (typeof element === 'string') ? document.querySelector(element) : element.node;
+        parent.appendChild(this.node);
+        return this;
     }
 }
